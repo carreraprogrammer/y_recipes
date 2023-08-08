2c4f7972722344c585a798b8f59ad6a69d180d9b
Daniel <carreraprogrammer@gmail.com>
2023-08-08 15:45:05 -0500

    Personalize autenthication routes and add home page

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,5 @@
+class LandingPagesController < ApplicationController
+  def home
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'landing_pages#home'
   devise_for :users, path: '', path_names: {
     sign_in: 'sign_in',
     sign_out: 'sign_out',
