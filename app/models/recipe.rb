b22d2becab33535638e3c25e4e3fa467d4c64e17
Daniel <carreraprogrammer@gmail.com>
2023-08-07 22:34:33 -0500

    Add validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, add_foreign_key: :user_id
+  # has_many :food
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :preparation_time_minutes, numericality: { greater_than_or_equal_to: 0 }
+  validates :cooking_time_minutes, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, presence: true
+  validates :description, length: { minimum: 10 }
+  validates :public, inclusion: { in: [true, false] }
 end
