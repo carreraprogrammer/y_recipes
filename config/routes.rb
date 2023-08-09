d2e27e3f5b693bb435a359bb36114b1ed3888a6a
Cisco <ciscophrenzy@gmail.com>
2023-08-09 06:24:39 -0600

    Uodate routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :recipes, only: [:index, :show, :new, :create, :destroy]
   end
+  resources :foods
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
